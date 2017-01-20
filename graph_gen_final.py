6541978ca0cddf39ad376a3633fd3e7917c9471a
rashmi2383 <rashmi23@bu.edu>
2017-01-20 15:45:49 -0500

    Update graph_gen_final.py

--- graph_gen_final.py
+++ graph_gen_final.py
@@ -1,11 +1,11 @@
 import numpy as np
 from collections import Counter, defaultdict
 import sys
-from copy import deepcopy
 import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import simplejson
 
 def if_float_value(float_number):
     try:
@@ -40,7 +40,7 @@ def draw_cache_miss_chart(dictdf, graph_algorithm, thread_count):
     plt.ylim([0, max(df['L1-I Cache Misses'] + df['L1-D Cache Misses'] + df['L2 Cache Misses'])] )
     # Adding the legend and showing the plot
     plt.legend(['L1-I Cache Miss Rate (Scaled by 40)', 'L1-D Cache Miss Rate (Scaled by 20)', 'L2 Cache Miss Rate'], loc='upper left')
-    plt.savefig('Cache Miss '+graph_algorithm)    
+    plt.savefig('Cache Miss '+graph_algorithm+' '+thread_count)    
 
 def draw_bar_chart(dictdftemp, graph_algorithm, thread_count):
     column_list = dictdftemp.keys()
@@ -64,6 +64,8 @@ def draw_bar_chart(dictdftemp, graph_algorithm, thread_count):
         ax.set_ylabel('Bits sent per second')
     if column_list[1] == 'Completion Time':
         ax.set_ylabel('Completion time (in nanoseconds)')
+    if column_list[1] == "CPU Performance":
+        ax.set_ylabel('Instructions per cycle')
     # Set the x axis label
     ax.set_xlabel('No. of Threads')
     # Set the chart's title
@@ -72,9 +74,10 @@ def draw_bar_chart(dictdftemp, graph_algorithm, thread_count):
 
               
 def main():
-    dict1, dicttemp, dictdf, dictdf4 = defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list)
-    dicttemplatency, dict1tempbits, dictdf2, dictdf3 = defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list)
-    listtmp1, listtmp2 = [], []
+    dict1, dicttemp, dictdf, dictdf4, dicttempInstr = defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list)
+    dicttemplatency, dict1tempbits, dictdf2, dictdf3, dictdf5 = defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list)
+    dictfile = defaultdict(list)
+    listtmp1, listtmp2, listtmp3 = [], [], []
     i, j, k = 0, 0 , 0
     # read command file in the result folder and extract the benchmark and command run details
     fileptr1 = "command"
@@ -130,7 +133,11 @@ def main():
                     if text == "Sent":  
                         dict1tempbits["Total bits sent"+str(k)].append(words)
                         k += 1
-    
+            # Store the total Instructions executed for each thread
+            if word == "Total":
+                for text in words:
+                    if text == "Instructions":
+                        dicttempInstr["Total Instructions"].append(words)
     del dict1tempbits["Total bits sent0"]                   
     del dicttemplatency["Packet Latency0"]
     del dicttemplatency["Packet Latency1"]
@@ -158,6 +165,17 @@ def main():
             listtmp2 = dict1[key]
     for i in range(0,len(listtmp1)):
         dict1["Throughput"].append((float(listtmp1[i])/listtmp2[i]) * (10**9))
+    del listtmp1
+    # Store total Instructions in the main dictionary & calculate IPC 
+    for values in dicttempInstr["Total Instructions"]:
+        for text in values:
+            if (text.isdigit()) and int(text) != 0:
+                dict1["Total Instructions"].append(int(text))
+    for key in dict1:
+        if key == "Total Instructions" in dict1:
+            listtmp1 = dict1[key]
+    for i in range(0,len(listtmp1)):
+        dict1["IPC"].append((float(listtmp1[i])/listtmp2[i]) * (10**9))                
     # Store L1-I Cache, L1-D Cache & L2 Cache Miss Rates in the main dictionary            
     for key in dicttemp:
         if key == "Cache Misses0":
@@ -180,25 +198,79 @@ def main():
     dict1["completion_time"].append(completion_time)
     for j in range(1, len(dict1["completion_time_threads"])+1):
         dict1["No_of_Threads"].append(j)
+    
+    i = 0    
+    if int(thread_count) > 16:
+        while(i < int(thread_count)):
+            listtmp3.append(i)
+            i = (i * 2) + 1
+      
     # generate the dictionary for data frame
     for key in dict1:
         if key=="No_of_Threads" in dict1:
-            dictdf[key]=dict1[key]
-            dictdf2[key]=dict1[key]
-            dictdf3[key]=dict1[key]
-            dictdf4[key]=dict1[key]
+            if int(thread_count) > 16:
+                for i in range(0,int(thread_count)):
+                    if i in listtmp3:
+                        dictdf[key].append(dict1[key][i])
+                        dictdf2[key].append(dict1[key][i])
+                        dictdf3[key].append(dict1[key][i])
+                        dictdf4[key].append(dict1[key][i])
+                        dictdf5[key].append(dict1[key][i])
+            else:
+                dictdf[key]=dict1[key]
+                dictdf2[key]=dict1[key]
+                dictdf3[key]=dict1[key]
+                dictdf4[key]=dict1[key]
+                dictdf5[key]=dict1[key]
         if key=="L1-I Cache Misses" in dict1:
-            dictdf[key]=dict1[key]
+            if int(thread_count) > 16:
+                for i in range(0,int(thread_count)):
+                    if i in listtmp3:
+                        dictdf[key].append(dict1[key][i])
+            else:
+                dictdf[key]=dict1[key]
         if key=="L1-D Cache Misses" in dict1:
-            dictdf[key]=dict1[key]
+            if int(thread_count) > 16:
+                for i in range(0,int(thread_count)):
+                    if i in listtmp3:
+                        dictdf[key].append(dict1[key][i])
+            else:
+                dictdf[key]=dict1[key]
         if key=="L2 Cache Misses" in dict1:
-            dictdf[key]=dict1[key]
+            if int(thread_count) > 16:
+                for i in range(0,int(thread_count)):
+                    if i in listtmp3:
+                        dictdf[key].append(dict1[key][i])
+            else:
+                dictdf[key]=dict1[key]
         if key=="Network Latency" in dict1:
-            dictdf2[key]=dict1[key]
+            if int(thread_count) > 16:
+                for i in range(0,int(thread_count)):
+                    if i in listtmp3:
+                        dictdf2[key].append(dict1[key][i])
+            else:
+                dictdf2[key]=dict1[key]
         if key=="Throughput" in dict1:
-            dictdf3[key]=dict1[key]
+            if int(thread_count) > 16:
+                for i in range(0,int(thread_count)):
+                    if i in listtmp3:
+                        dictdf3[key].append(dict1[key][i])
+            else:
+                dictdf3[key]=dict1[key]
         if key=="completion_time_threads" in dict1:
-            dictdf4["Completion Time"]=dict1[key]
+            if int(thread_count) > 16:
+                for i in range(0,int(thread_count)):
+                    if i in listtmp3:
+                        dictdf4["Completion Time"].append(dict1[key][i])
+            else:
+                dictdf4["Completion Time"]=dict1[key]
+        if key=="IPC" in dict1:
+            if int(thread_count) > 16:
+                for i in range(0,int(thread_count)):
+                    if i in listtmp3:
+                        dictdf5["CPU Performance"].append(dict1[key][i])
+            else:
+                dictdf5["CPU Performance"]=dict1[key]
 
     # Call the function to plot the bar chart for Cache Miss Rates
     draw_cache_miss_chart(dictdf, graph_algorithm, thread_count)
@@ -208,6 +280,18 @@ def main():
     draw_bar_chart(dictdf3, graph_algorithm, thread_count)
     # Call the function to plot the bar chart for completion times
     draw_bar_chart(dictdf4, graph_algorithm, thread_count)
+    # Call the function to plot the bar chart for completion times
+    draw_bar_chart(dictdf5, graph_algorithm, thread_count)
 
+    
+    dictfile = dict(dictdf.items() + dictdf2.items() + dictdf3.items() + dictdf4.items() + dictdf5.items()) 
+    dictfile[graph_algorithm] = thread_count
+
+    f = open('statistics.txt', 'a')
+    f.write(graph_algorithm)
+    f.write(thread_count)
+    simplejson.dump(dictfile, f)
+    f.close()
+    
 if __name__ == '__main__':
     main()   
